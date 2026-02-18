418e214d661cda7ce1555ff0e11ea48560217b3a
Lorenzon <juniorlorenzonvaldemar@gmail.com>
2026-02-18 14:43:19 -0300

    Add uso de argumentos ajustes

--- src/doCproJava2026-1/L_Argumentos/src/Argumentos.java
+++ src/doCproJava2026-1/L_Argumentos/src/Argumentos.java
@@ -1,25 +1,26 @@
 public class Argumentos {
-    public static void main(String[] args) {
+    public static void main(String[] vetorArgumentos) {
 
-        if (args.length == 0) {
+        if (vetorArgumentos.length == 0) {
             System.out.println("Nenhum argumento foi informado.");
             System.out.println("Exemplo de uso: java Main Ana 10 --modo=teste");
             return;
         }
 
-        System.out.println("Quantidade de argumentos: " + args.length);
+        System.out.println("Quantidade de argumentos: " + vetorArgumentos.length);
         System.out.println("Argumentos recebidos:");
 
         // For em modo trivial
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("- " + args[i]);
+        for (int i = 0; i < vetorArgumentos.length; i++) {
+            System.out.println("- " + vetorArgumentos[i]);
         }
 
         System.out.println("Argumentos recebidos, novamente:");
 
-        // For em modo each
-        for (String arg : args) {
-            System.out.println("- " + arg);
+        // For em modo each: para cada item da coleção, iterado automaticamente pois
+        // implementa Interface Iterator
+        for (String argumeto : vetorArgumentos) {
+            System.out.println("- " + argumeto);
         }
     }
 }
